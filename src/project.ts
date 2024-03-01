e1f1e0282c681a33566ad391344e253420122edc
Никита Табакаев <root.kidik@gmail.com>
2024-03-01 22:08:13 +0700

    feat: add sentiniel_search

--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,22 @@
 import { makeProject } from "@motion-canvas/core";
 
+/* linear
 import idea from "./scenes/searching/linear/idea?scene";
 import asymptotic from "./scenes/searching/linear/asymptotic?scene";
 import code from "./scenes/searching/linear/code?scene";
+*/
+
+import idea from "./scenes/searching/sentinel_linear/idea?scene";
+import asymptotic from "./scenes/searching/sentinel_linear/asymptotic?scene";
+import code from "./scenes/searching/sentinel_linear/code?scene";
 
 import "./global.css";
+
+/* linear
+export default makeProject({
+  scenes: [idea, asymptotic, code],
+});
+*/
 
 export default makeProject({
   scenes: [idea, asymptotic, code],
